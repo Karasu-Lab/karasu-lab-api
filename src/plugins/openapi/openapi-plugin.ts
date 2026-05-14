7e3fcc7ae8690dda797168936cbf1185885673a0
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2026-05-14 12:03:44 +0900

    refactor: simplify openAPI plugin and update Dockerfile for package copying

--- src/plugins/openapi/openapi-plugin.ts
+++ src/plugins/openapi/openapi-plugin.ts
@@ -1,30 +1,6 @@
 import { BetterAuthPlugin } from 'better-auth';
-import { openAPI as defaultOpenAPI } from 'better-auth/plugins';
-import { AbstractPluginEnvironment } from '../../shared/plugin/abstract-plugin-environment.js';
-import { Environment } from '@hashibutogarasu/common';
-
-class ProductionOpenAPIEnvironment extends AbstractPluginEnvironment {
-  resolve(): BetterAuthPlugin {
-    return defaultOpenAPI();
-  }
-}
-
-class DevelopmentOpenAPIEnvironment extends AbstractPluginEnvironment {
-  resolve(): BetterAuthPlugin {
-    return defaultOpenAPI();
-  }
-}
-
-class TestOpenAPIEnvironment extends AbstractPluginEnvironment {
-  resolve(): BetterAuthPlugin {
-    return defaultOpenAPI();
-  }
-}
+import { openAPI } from 'better-auth/plugins';
 
 export const openAPIPlugin = (): BetterAuthPlugin => {
-  return AbstractPluginEnvironment.resolve({
-    [Environment.PRODUCTION]: ProductionOpenAPIEnvironment,
-    [Environment.DEVELOPMENT]: DevelopmentOpenAPIEnvironment,
-    [Environment.TEST]: TestOpenAPIEnvironment,
-  });
+  return openAPI();
 };
